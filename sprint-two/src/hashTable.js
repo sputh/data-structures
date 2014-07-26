0c63a6c1b8db310d58671d47c6bafdf40b46539d
sputh <samantha.puth@gmail.com>
2014-07-25 21:14:03 -0700

    Passed 4/5 specs for Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = [];
+  this._storage[i].push({k:v});
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  // Access storage at [i]
+  // check to see if it is a bucket or not
+  if (this._storage[i].length = 1) {
+    return this._storage[i][0].k;
+  } else {
+    for ( var j = 0; j < this._storage[i].length; j++) {
+      if(k === Object.keys(this._storage[i][j])) {
+        return this._storage[i][j][k];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
+    var i = getIndexBelowMaxForKey(k, this._limit);
+  // Access storage at [i]
+  // check to see if it is a bucket or not
+  if (this._storage[i].length = 1) {
+    this._storage[i][0].k = null;
+  } else {
+    for ( var j = 0; j < this._storage[i].length; j++) {
+      if(k === Object.keys(this._storage[i][j])) {
+        this._storage[i][j][k] = null;
+      }
+    }
+  }
 
 };
 
