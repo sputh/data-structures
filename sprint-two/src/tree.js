72afd7e50ca2084a694db0676ec7d4c690d75add
sputh <samantha.puth@gmail.com>
2014-07-25 19:31:50 -0700

    Completed Trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,10 +12,23 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var tree = makeTree(value);
+  this.children.push(tree);
 
 };
 
 treeMethods.contains = function(target){
+  if (this.value === target) {
+    return true;
+  } else if (this.children) {
+    for (var i = 0; i < this.children.length; i ++) {
+      var child = this.children[i];
+      if(child.contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 
 };
 
