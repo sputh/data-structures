fd780529bd33838d5c6ea9b2c5e5622a1f81c6be
sputh <samantha.puth@gmail.com>
2014-07-26 13:43:25 -0700

    Completed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,40 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = [];
-  this._storage[i].push({k:v});
-
+  if (this._storage.get(i)) {
+    this._storage.get(i).push([k,v]);
+  } else {
+    this._storage.set(i, []);
+    this._storage.get(i).push([k,v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // Access storage at [i]
-  // check to see if it is a bucket or not
-  if (this._storage[i].length = 1) {
-    return this._storage[i][0].k;
-  } else {
-    for ( var j = 0; j < this._storage[i].length; j++) {
-      if(k === Object.keys(this._storage[i][j])) {
-        return this._storage[i][j][k];
-      }
+  for (var j = 0; j < this._storage.get(i).length; j++) {
+    if (this._storage.get(i)[j][0] === k) {
+      return this._storage.get(i)[j][1];
     }
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-    var i = getIndexBelowMaxForKey(k, this._limit);
-  // Access storage at [i]
-  // check to see if it is a bucket or not
-  if (this._storage[i].length = 1) {
-    this._storage[i][0].k = null;
-  } else {
-    for ( var j = 0; j < this._storage[i].length; j++) {
-      if(k === Object.keys(this._storage[i][j])) {
-        this._storage[i][j][k] = null;
-      }
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  for (var j = 0; j < this._storage.get(i).length; j++) {
+    if (this._storage.get(i)[j][0] === k) {
+      this._storage.get(i).splice(j,1);
+
     }
   }
-
 };
 
 
